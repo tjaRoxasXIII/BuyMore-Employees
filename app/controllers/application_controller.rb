b43d69762b9467a2283231ec2fa4af626814e0ca
tjaRoxasXIII <tjaroguetech@gmail.com>
2020-10-07 10:02:35 -0400

    Added update 'edit' and 'delete' functionality.  Included helper methods for session tracking.  Adjusting signup page for completeness of user creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,19 +11,34 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :index
+    if Helpers.is_logged_in?(session)
+      redirect '/home'
+    else
+      erb :index
+    end
   end
 
   get '/employees/new' do
-    erb :"/employees/new"
+    if Helpers.is_logged_in?(session)
+      redirect '/home'
+    else
+      erb :"/employees/new"
+    end
   end
 
   post '/employees/new' do
     @employee = Employee.find_by(email: params[:email])
     if @employee == nil
+      params.each do |param|
+        if param[1].empty?
+          redirect '/employees/new'
+        else
+        end
+      end
       new_employee = Employee.new(first_name: params[:first_name], last_name: params[:last_name], job_title: params[:job_title], email: params[:email], birthdate: params[:birthdate], password: params[:password])
       new_employee.store_id = Store.find_by(location: params[:job_location]).id
       new_employee.save
+      session[:id] = @employee.id
       redirect "/home"
     else
       redirect '/employees/new'
@@ -57,5 +72,35 @@ class ApplicationController < Sinatra::Base
     @update.save
     # binding.pry
     redirect "/home"
+  end
+
+  get '/updates/:id/edit' do
+    # binding.pry
+    @update = Update.find_by(id: params[:id])
+    if @update.employee_id == session[:id]
+      erb :"/updates/edit"
+    else
+      redirect '/home'
+    end
+    
+  end
+
+  patch '/updates/:id' do
+    binding.pry
+    @update = Update.find_by(id: params[:id])
+    @update.content = params[:content]
+    @update.save
+    redirect '/home'
+  end
+
+  get '/updates/:id/delete' do
+    @update = Update.find_by(id: params[:id])
+    @update.delete
+    redirect '/home'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
   end
 end
