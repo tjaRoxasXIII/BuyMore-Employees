b342e8c8bf1f75b9f91edd8a0ae970ccfebeb6dc
tjaRoxasXIII <tjaroguetech@gmail.com>
2020-10-14 11:00:36 -0400

    Refactored employees to use validations and adjusted the employee_controller accordingly

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -9,17 +9,9 @@ class EmployeeController < ApplicationController
     end
 
     post '/employees/new' do
-        @employee = Employee.find_by(email: params[:email])
-        if @employee == nil
-            params.each do |param|
-            if param[1].empty?
-                redirect '/employees/new'
-            else
-            end
-            end
-            new_employee = Employee.new(first_name: params[:first_name], last_name: params[:last_name], job_title: params[:job_title], email: params[:email], birthdate: params[:birthdate], password: params[:password])
-            new_employee.store_id = Store.find_by(location: params[:job_location]).id
-            new_employee.save
+        new_employee = Employee.new(params[:employee])
+        new_employee.store_id = Store.find_by(location: params[:job_location]).id
+        if new_employee.save
             session[:id] = new_employee.id
             redirect "/home"
         else
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,8 @@
 class Employee < ActiveRecord::Base
     belongs_to :store
     has_many :updates
+    validates :email, uniqueness: :true
+    validates :first_name, :last_name, :job_title, :birthdate, presence: :true
     has_secure_password
 
     def full_name
